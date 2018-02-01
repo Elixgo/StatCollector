df6c9de0b683484ee54ceb4f7f8eff295ef945e3
NorbertKa <mievstac@gmail.com>
2018-02-01 02:31:36 +0200

    Added Pretty printing to Config struct

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"errors"
 	"github.com/BurntSushi/toml"
+	"log"
 )
 
 const DEFAULT_CONFIG_LOCATION = "./config.toml"
@@ -18,6 +19,12 @@ type Config struct {
 	Logging bool
 }
 
+func (c Config) Print() {
+	log.Printf("Report Interval: %vms", c.ReportInterval)
+	log.Printf("Server address: %v:%v", c.ServerHost, c.ServerPort)
+	log.Printf("Logging: %t", c.Logging)
+}
+
 func ReadConfig(fileLocation string) (*Config, error) {
 	fileDataRaw, err := ioutil.ReadFile(fileLocation)
 	if err != nil {
@@ -28,7 +35,7 @@ func ReadConfig(fileLocation string) (*Config, error) {
 	}
 	fileData := string(fileDataRaw)
 	var conf Config
-	if _, err :=  toml.Decode(fileData, &conf); err != nil {
+	if _, err := toml.Decode(fileData, &conf); err != nil {
 		return nil, err
 	}
 	return &conf, nil
--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(config)
+	config.Print()
 }
